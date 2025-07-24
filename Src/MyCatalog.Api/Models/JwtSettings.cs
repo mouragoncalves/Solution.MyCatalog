303ef7ea528c505d5aa4c73f9e772eac348ca099
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2025-07-24 06:39:38 -0300

    Config JwtBearer

---
+++ Src/MyCatalog.Api/Models/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace MyCatalog.Api.Models
+{
+    public class  JwtSettings
+    {
+        public string Segredo { get; set; } = string.Empty;
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; } = string.Empty;
+        public string ValidoEm { get; set; } = string.Empty;
+    }
+}
--- Src/MyCatalog.Api/Program.cs
+++ Src/MyCatalog.Api/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MyCatalog.Api.Data;
+using MyCatalog.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +20,35 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApiDbContext>();
     //.AddDefaultTokenProviders();
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+builder.Services.Configure<JwtSettings>(jwtSettings);
+
+var jwtOptions = jwtSettings.Get<JwtSettings>() ?? throw new Exception("JWT settings are not configured properly.");
+
+var key = System.Text.Encoding.ASCII.GetBytes(jwtOptions.Segredo);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = true;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidIssuer = jwtOptions.Emissor,
+            ValidateAudience = true,
+            ValidAudience = jwtOptions.ValidoEm,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero // Remove a tolerância de tempo padrão de 5 minutos
+        };
+    });
 
 var app = builder.Build();
 
